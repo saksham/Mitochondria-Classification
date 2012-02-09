3bac50ef1f32a87b7e1e86d81c1fbf8a6955f8c6
Saksham Gautam <saksham.gautam@gmail.com>
2012-02-09 17:58:30 +0100

    Added 'hidden layer size' to the neural network structure
    * Refactored the structure and added more properties
    * The additional properties are useful for 'rolling/unrolling'

--- nnCreate.m
+++ nnCreate.m
@@ -1,9 +1,12 @@
-function [ nn ] = nnCreate(input_dim, output_dim, hidden_layer_nodes_count)
+function [ nn ] = nnCreate(input_dim, output_dim, hidden_layer_sizes)
 %NNCREATE Creates a neural network struct based on the given parameters
-% [nn]  =   nnCreate(input_dim, output_dim, hidden_layers_nodes_count)
+% [nn]  =   nnCreate(input_dim, output_dim, hidden_layer_sizes)
 %           returns a neural network structure. The structure contains the
 %           following properties.
-%           layers_count:   The number of hidden layers in the network.
+%           hidden_layers_count: The number of hidden layers in the network
+%           layers_count: The total number of layers in the network
+%           layer_sizes: The number of nodes in all layers including the
+%                        input and output layers
 %           theta:  An array containing weight matrix for each hidden layer
 %           
 %           The structure will be based on the following input parameters.
@@ -12,26 +15,20 @@ function [ nn ] = nnCreate(input_dim, output_dim, hidden_layer_nodes_count)
 %           output_dim: the number of output units, which corresponds to
 %                       the number of classes the neural network is
 %                       supposed to identify.
-%           hidden_layer_nodes_count: An array that denotes the number of 
-%                                     nodes in the hidden layers. The
-%                                     length of the array indicates the
-%                                     number of the number of hidden layers
+%           hidden_layer_sizes: An array that denotes the number of nodes 
+%                               in the hidden layers. The length of the
+%                               array indicates the number of hidden
+%                               layers.
 
-nn.layers_count = length(hidden_layer_nodes_count);
+nn.hidden_layers_count = numel(hidden_layer_sizes);
+nn.layers_count = 2 + nn.hidden_layers_count;
+nn.layer_sizes = [input_dim, hidden_layer_sizes, output_dim];
+
 EPSILON_INIT = 0.12;
 
-for i = 1:(nn.layers_count + 1)
-    if (i == 1)
-        rows = input_dim + 1;
-    else
-        rows = hidden_layer_nodes_count(i - 1) + 1;
-    end
-    
-    if(i > nn.layers_count)
-        cols = output_dim;
-    else
-        cols = hidden_layer_nodes_count(i);
-    end
+for i = 1:(nn.layers_count - 1)
+    rows = nn.layer_sizes(i) + 1;
+    cols = nn.layer_sizes(i + 1);
     nn.theta{i} = rand(rows, cols) * 2 * EPSILON_INIT - EPSILON_INIT;
     
 end
