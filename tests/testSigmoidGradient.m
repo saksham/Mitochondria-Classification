25afc148a0bfb696dd3e3cc60f3e391a2bb9827f
Saksham Gautam <saksham.gautam@gmail.com>
2012-02-09 19:26:47 +0100

    Implemented test for sigmoidGradient function

---
+++ tests/testSigmoidGradient.m
@@ -0,0 +1,11 @@
+function testSigmoidGradient
+%TESTSIGMOIDGRADIENT Tests the sigmoidGradient function
+
+in = [1 -0.5 0 0.5 1];
+out = sigmoidGradient(in);
+expected = [0.196611933241482, 0.235003712201594, 0.250000000000000, ...
+    0.235003712201594, 0.196611933241482];
+assertElementsAlmostEqual(expected, out);
+
+end
+
