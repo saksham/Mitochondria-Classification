c0daeec713a7343c1725d9392575d1cd017da06f
Daniel Hein <daniel.hein@tum.de>
2012-02-23 18:00:16 +0100

    SVM test

--- tests/testSVMGetVectorsKernel.m
+++ tests/testSVMGetVectorsKernel.m
@@ -25,44 +25,56 @@ addpath(str);
 % assertEqual(new_y , [-1 ; 1]);
 
 load('data/ml-class_ex4data1.mat');
+C = 150;
+features = 12;
 
 % do a little pPCA to reduce the amount of parameters from 400 to only 10
-X = pPCA(X,10);
+X = pPCA(X,features);
 
 % multi class is too difficult ;-)
 % thats only predict for '3' for testing
-% train on 100 samples
-n = length(y);
-y_next = zeros(100,1);
-X_next = zeros(100,10);
-for i = 1 : 50
-    if y(i*50) == 3
-        y_next(i) = 1;
-    else
-        y_next(i) = -1;
-    end
-    X_next(i,:) = X(i*50,:);
-end
+% train on 475 samples
+% 250 - not 3
+% 225 - 3
+y_next = zeros(475,1);
+X_next = zeros(475,features);
+X_next(1:25,:) = X(1:25,:);         %10
+X_next(26:50,:) = X(501:525,:);     %1
+X_next(51:75,:) = X(1001:1025,:);   %2
+X_next(76:100,:) = X(2001:2025,:);  %4
+X_next(101:125,:) = X(2501:2525,:); %5
+X_next(126:150,:) = X(3001:3025,:); %6
+X_next(151:175,:) = X(3501:3525,:); %7
+X_next(176:200,:) = X(4001:4025,:); %8
+X_next(201:225,:) = X(4501:4525,:); %9
+X_next(226:475,:) = X(1501:1750,:); %3
+y_next(1:225) = -1;
+y_next(226:475) = 1;
 
-[SV_X , SV_y] = svmGetVectorsKernel(X_next,y_next,0,10,'gauss',1,0);
+[SV_X , SV_y] = svmGetVectorsKernel(X_next,y_next,0,C,'gauss',1,0);
 
-% predict for 50 samples
-n = length(y);
-y_next = zeros(50,1);
-X_next = zeros(50,10);
-for i = 1 : 50
-    if y(i*100-3) == 3
-        y_next(i) = 1;
-    else
-        y_next(i) = -1;
-    end
-    X_next(i,:) = X(i*100-3,:);
-end
+% test on 475 samples
+% 250 - not 3
+% 225 - 3
+y_next = zeros(475,1);
+X_next = zeros(475,features);
+X_next(1:25,:) = X(26:50,:);         %10
+X_next(26:50,:) = X(526:550,:);     %1
+X_next(51:75,:) = X(1026:1050,:);   %2
+X_next(76:100,:) = X(2026:2050,:);  %4
+X_next(101:125,:) = X(2526:2550,:); %5
+X_next(126:150,:) = X(3026:3050,:); %6
+X_next(151:175,:) = X(3526:3550,:); %7
+X_next(176:200,:) = X(4026:4050,:); %8
+X_next(201:225,:) = X(4526:4550,:); %9
+X_next(226:475,:) = X(1751:2000,:); %3
+y_next(1:225) = -1;
+y_next(226:475) = 1;
 
-pred = svmClassifier(SV_X,SV_y,X_next,10,'gauss',1,0);
+pred = svmClassifier(SV_X,SV_y,X_next,C,'gauss',1,0);
 
 trainingSetAccuracy = mean(double(pred == y_next)) * 100;
-assert(trainingSetAccuracy > 93, 'Training set accuracy should be ~95%');
+assert(trainingSetAccuracy > 90, 'Training set accuracy should be ~90%');
 
 end
 
