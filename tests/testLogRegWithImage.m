60ccce78c31291c80c0541f083947f6f39df3315
Saksham Gautam <saksham.gautam@gmail.com>
2012-03-08 12:47:30 +0100

    * Taking average of features in image

--- tests/testLogRegWithImage.m
+++ tests/testLogRegWithImage.m
@@ -6,31 +6,28 @@ images = images_struct(1).('images');
 y_vec_struct = load('data/y_vec.mat');
 y_vec = y_vec_struct(1).('y_vec');
 
-% Change the labels to {0, 1} instead of {-1, 1}
-y_vec = (y_vec + 1) / 2;
 
-X = [];
-y = [];
+m = size(images, 2);
+n = 2;
+X = ones(m, n + 1);
+y = (y_vec + 1 ) / 2;  % Change the labels to {0, 1} instead of {-1, 1}
 for i = 1:size(images, 2)
     I = images{i};
     blobs = blobVector(I, 0.84, 0.11);
     x_i = featureExtractionBoundaries(blobs);
-    X = [X; x_i];
-    
-    % The labels for the entire image will be the label for each mitochondria
-    y_i = repmat(y_vec(i), size(x_i, 1), 1);
-    y = [y; y_i];
+    X(i, 2:end) = mean(x_i);
 end
-X = [ones(size(X, 1), 1) X];
 
-plot2dDataWithLabels(X, y);
 initial_theta = zeros(size(X, 2), 1);
 lambda = 0;
 
 % Set options and optimize
-options = optimset('GradObj', 'on', 'MaxIter', 400);
+options = optimset('GradObj', 'on', 'MaxIter', 500);
 [theta, J, exit_flag] = ...
 	fminunc(@(t)(logRegCostFunction(t, X, y, lambda)), initial_theta, options);
+
+% Plot decision boundary
+plot2dDecisionBoundary(theta, X, y);
 
 % Compute accuracy on our training set
 p = logRegPredict(theta, X);
